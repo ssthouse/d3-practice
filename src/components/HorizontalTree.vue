563933b4b214371b6d376d515c691a5fd6c6e97f
shen_s <ssthouse@163.com>
2018-03-30 16:31:19 +0800

    Add HorizontalTree menu.

---
+++ src/components/HorizontalTree.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <svg id="horizontalTreeSvg"></svg>
+  <h1>hi this is test component</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HorizontalTree',
+  data () {
+    return {
+      g: null
+    }
+  },
+  methods: {
+
+  },
+  mounted () {
+    const svg = this.$d3.select('horizontalTreeSvg')
+    this.g = svg.append('g')
+      .attr('transform', 'translate(100, 100)')
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
