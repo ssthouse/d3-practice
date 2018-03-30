f858956cd26943521dc04ece0722b2e759de998b
ssthouse <ssthouse@163.com>
2018-03-30 12:11:56 +0800

    Finish simple update pattern.

--- src/components/Root.vue
+++ src/components/Root.vue
@@ -11,30 +11,42 @@ export default {
   data () {
     return {
       alphabet: 'abcdefghijklmnopqrstuvwxyz'.split(''),
-      g: null
+      g: null,
+      transition: null
     }
   },
   methods: {
     getRandomStr () {
       return this.$d3.shuffle(this.alphabet)
-        .slice(0, Math.floor(Math.random() * 26))
+        .slice(0, Math.floor(Math.random() * 16 + 10))
         .sort()
+    },
+    getTransition () {
+      return this.$d3.transition()
+        .duration(2000)
     },
     update (charArray) {
       const selection = this.g.selectAll('text')
         .data(charArray, d => d)
 
-      selection.attr('class', 'update')
+      selection.transition(this.getTransition())
+        .duration(2000)
+        .attr('class', 'update')
+        .attr('x', (d, i) => i * 42)
 
       selection.enter()
         .append('text')
         .attr('class', 'enter')
-        .merge(selection)
-        .attr('x', (d, i) => i * 32)
-        .attr('dy', '.35em')
+        .transition(this.getTransition())
+        .attr('x', (d, i) => i * 42)
+        .attr('dy', '1em')
         .text(d => d)
 
       selection.exit()
+        .attr('class', 'exit')
+        .transition(this.getTransition())
+        .attr('y', 0)
+        .style('fill-opacity', 1e-6)
         .remove()
     },
     start () {
@@ -42,13 +54,15 @@ export default {
       this.update(this.alphabet)
       this.$d3.interval(function () {
         self.update(self.getRandomStr())
-      }, 1500)
+      }, 3000)
     }
   },
   mounted () {
     const mainSvg = this.$d3.select('svg')
     this.g = mainSvg.append('g')
       .attr('transform', 'translate(32, 200)')
+    this.transition = this.$d3.transition()
+      .duration(1000)
   }
 }
 </script>
@@ -56,8 +70,8 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
 
-  .text {
-    font: bold 120px monospace;
+  text {
+    font: bold 48px monospace;
   }
 
   .enter {
