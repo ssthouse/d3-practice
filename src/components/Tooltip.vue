46ab6f4684cf36e4ce13feb902a53e432dfe6cbd
shen_s <ssthouse@163.com>
2018-04-03 10:45:35 +0800

    Add padding.

--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -11,22 +11,32 @@ export default {
   name: 'Tooltip',
   data() {
     return {
+      padding: 20,
+      width: 600,
+      height: 300,
       g: null,
       data: [],
       parseDate: this.$d3.timeParse('%d-%b-%y'),
       formatTime: this.$d3.timeFormat('%e %B'),
-      xScale: this.$d3.scaleTime().range([0, 600]),
-      yScale: this.$d3.scaleLinear().range([300, 0])
+      xScale: null,
+      yScale: null
     }
   },
   methods: {
     start() {
+      this.xScale = this.$d3
+        .scaleTime()
+        .range([this.padding, this.width - this.padding])
+      this.yScale = this.$d3
+        .scaleLinear()
+        .range([this.height - this.padding, this.padding])
+
       const xAxis = this.$d3
-        .axisBottom([0, 600])
+        .axisBottom([this.padding, this.width - this.padding])
         .scale(this.xScale)
         .ticks(5)
       const yAxis = this.$d3
-        .axisLeft([0, 300])
+        .axisLeft([this.padding, this.height - this.padding])
         .scale(this.yScale)
         .ticks(5)
 
@@ -49,12 +59,13 @@ export default {
       this.g
         .append('g')
         .attr('class', 'x axis')
-        .attr('transform', 'translate(0, 300)')
+        .attr('transform', 'translate(0, ' + (this.height - this.padding) + ')')
         .call(xAxis)
 
       this.g
         .append('g')
         .attr('class', 'y axis')
+        .attr('transform', 'translate(' + this.padding + ',0)')
         .call(yAxis)
     },
     parseCsvData() {
