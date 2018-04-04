6b64a66b8247d45a254a359879624f7571d380f9
shen_s <ssthouse@163.com>
2018-04-04 10:57:35 +0800

    Add simple bar chart.

--- src/components/HorizontalTree.vue
+++ src/components/HorizontalTree.vue
@@ -1,29 +1,60 @@
 <template>
-<div>
+<div id="horizontal-tree">
   <svg id="horizontalTreeSvg"></svg>
-  <h1>hi this is test component</h1>
 </div>
 </template>
 
 <script>
 export default {
   name: 'HorizontalTree',
-  data () {
+  data() {
     return {
+      barValues: [1, 33, 5, 9, 7, 5, 3, 2],
+      barWidth: 30,
+      width: 600,
+      height: 360,
       g: null
     }
   },
   methods: {
+    start() {
+      const barSelection = this.$d3
+        .select('#horizontal-tree')
+        .selectAll('.bar')
+        .data(this.barValues)
 
+      barSelection
+        .enter()
+        .append('div')
+        .attr('class', 'bar')
+        .style('width', this.barWidth + 'px')
+        .style('height', d => d * 10 + 'px')
+        .style('left', (d, i) => i * this.barWidth + 'px')
+
+      barSelection.exit().remove()
+    }
   },
-  mounted () {
+  mounted() {
     const svg = this.$d3.select('horizontalTreeSvg')
-    this.g = svg.append('g')
-      .attr('transform', 'translate(100, 100)')
+    this.g = svg.append('g').attr('transform', 'translate(30, 30)')
+
+    this.start()
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="less">
+#horizontal-tree {
+  position: relative;
+
+  #horizontalTreeSvg {
+    margin: 30px;
+  }
+
+  .bar {
+    position: absolute;
+    background-color: teal;
+  }
+}
 </style>
