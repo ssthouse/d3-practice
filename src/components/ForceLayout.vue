e6c8bdcc216706d725b8da182be25849d3d31749
ssthouse <ssthouse@163.com>
2018-04-26 19:54:36 +0800

    Commit code

---
+++ src/components/ForceLayout.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="force-layout" style="position: rkelative; width: 100%; height: 100%;">
+    <svg id="force-layout-svg"></svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tree',
+  data() {
+    return {
+      padding: 40,
+      width: 600,
+      height: 700,
+      value: [
+       
+      ],
+      g: null,
+      values: [],
+      xScale: null,
+      yScale: null
+    }
+  },
+  methods: {
+    initChartContainer() {
+      this.g = this.$d3
+        .select('#force-layout-svg')
+        .append('g')
+        .attr('transform', `translate(100, 100)`)
+    },
+    start() {
+    }
+  },
+  mounted() {
+    this.initChartContainer()
+    this.start()
+    window.vue = this
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang='less'>
+#force-layout {
+  #force-layout-svg {
+    padding-left: 30px;
+    padding-top: 30px;
+    width: 100%;
+    height: 100%;
+  }
+
+  circle {
+    fill: teal;
+  }
+
+  .link {
+    stroke: teal;
+  }
+
+  .label {
+    fill: white;
+  }
+}
+</style>
